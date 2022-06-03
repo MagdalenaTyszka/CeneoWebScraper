cfe4104af8332ec347cbb8fb02a3ac24264f18c2
Magdalena Tyszka <92882974+MagdalenaTyszka@users.noreply.github.com>
2022-06-03 20:00:13 +0200

    2022-06-03 MT

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from app import app
 import os
 import json
@@ -5,7 +6,7 @@ import requests
 import pandas as pd
 from matplotlib import pyplot as plt
 from bs4 import BeautifulSoup
-from flask import render_template
+from flask import render_template, redirect, url_for,request
 
 def extract_element(ancestor, selector, attribute=None, return_list=False):
     try:
@@ -37,36 +38,38 @@ def index():
     return render_template("index.html.jinja")
 @app.route('/author')    
 def author():
-    return "Magdalena Tyszka, Informatyka stosowana, N2"   
+    return render_template("author.html.jinja") 
 
-@app.route('/extract/<product_id>')
-def extract(product_id):
-    url = f"https://www.ceneo.pl/{product_id}#tab=reviews"
-    all_reviews = []
-    while(url):
-        response = requests.get(url)
-        page_dom = BeautifulSoup(response.text, 'html.parser')
-        reviews = page_dom.select("div.js_product-review")
-        for review in reviews:
-            single_review = {key:extract_element(review, *values)
-                            for key, values in review_elements.items()}
-            single_review["review_id"] = review["data-entry-id"]
+@app.route('/extract', methods = ["POST", "GET"])
+def extract():
+    if request == ["POST"]
+        url = f"https://www.ceneo.pl/{product_id}#tab=reviews"
+        all_reviews = []
+        while(url):
+            response = requests.get(url)
+            page_dom = BeautifulSoup(response.text, 'html.parser')
+            reviews = page_dom.select("div.js_product-review")
+            for review in reviews:
+                single_review = {key:extract_element(review, *values)
+                                for key, values in review_elements.items()}
+                single_review["review_id"] = review["data-entry-id"]
 
-            single_review["recommendation"] = True if single_review["recommendation"] == "Polecam" else False if single_review["recommendation"] == "Nie polecam" else None
-            single_review["stars"] = float(single_review["stars"].split("/").pop(0).replace(",", "."))
-            single_review["useful"] = int(single_review["useful"])
-            single_review["useless"] = int(single_review["useless"])
-            single_review["publish_date"] = single_review["publish_date"].split(" ").pop(0) if single_review["publish_date"] is not None else None
-            single_review["purchase_date"] = single_review["purchase_date"].split(" ").pop(0) if single_review["purchase_date"] is not None else None
-            all_reviews.append(single_review)
-        try: 
-            next_page = page_dom.select_one("a.pagination__next")
-            url = "https://www.ceneo.pl"+next_page["href"]
-        except TypeError: url = None    
+                single_review["recommendation"] = True if single_review["recommendation"] == "Polecam" else False if single_review["recommendation"] == "Nie polecam" else None
+                single_review["stars"] = float(single_review["stars"].split("/").pop(0).replace(",", "."))
+                single_review["useful"] = int(single_review["useful"])
+                single_review["useless"] = int(single_review["useless"])
+                single_review["publish_date"] = single_review["publish_date"].split(" ").pop(0) if single_review["publish_date"] is not None else None
+                single_review["purchase_date"] = single_review["purchase_date"].split(" ").pop(0) if single_review["purchase_date"] is not None else None
+                all_reviews.append(single_review)
+            try: 
+                next_page = page_dom.select_one("a.pagination__next")
+                url = "https://www.ceneo.pl"+next_page["href"]
+            except TypeError: url = None    
 
-    with open(f"./app/reviews/{product_id}.json", "w", encoding="UTF-8") as f:
-        json.dump(all_reviews, f, indent=4, ensure_ascii=False)
-    return json.dumps(all_reviews, indent=4, ensure_ascii=False)
+        with open(f"./app/reviews/{product_id}.json", "w", encoding="UTF-8") as f:
+            json.dump(all_reviews, f, indent=4, ensure_ascii=False)
+        return redirect(url_for('product', product))
+    else:
 
 @app.route('/products')
 def products():
